08d53e831da70f7ce70c1d96ef1404d8ac3d62b3
mistaj-ok <nickfury.jones@gmail.com>
2023-05-19 07:17:23 -0400

    working react app

--- src/containers/featured/Featured.jsx
+++ src/containers/featured/Featured.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import './featured.css';
-import feature1 from '../../assets/feature1.png';
-import feature2 from '../../assets/feature2.jpg';
 
 
 
@@ -14,13 +12,11 @@ const Feature = () => {
         <div className='image-boxes'>
           <div className='img-box small'>
             <p>image 1</p>
-            <img src={feature1} alt="ft1" />
           </div>
 
 
           <div className='img-box small'>
             <p>image 2</p>
-            <img src={feature2} alt="ft1" />
           </div>
 
 
